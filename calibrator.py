c6d3b5b014ce9a8ed7b8cf72d494e8910b490985
Andre <andreferraz@gwu.edu>
2025-07-09 18:22:37 -0400

    calibration code written

---
+++ calibrator.py
@@ -0,0 +1,44 @@
+import numpy as np 
+import cv2 
+
+# Definition of CheckerBoard Dimensions
+CHECKERBOARD = (6,9)
+objpoints = []
+imgpoints = []
+
+criteria = (cv2.TERM_CRITERIA_EPS +cv2.TERM_CRITERIA_MAX_ITER, 309, 0.001)
+
+# numpy array to store 3D coordinates
+objp = np.zeros((1,CHECKERBOARD[0]*CHECKERBOARD[1], 3), np.float32)
+objp[0,:,:2] = np.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1,2)
+
+
+for i in range(1,8):
+
+    image = cv2.imread(f"calibration_img{i}.jpg")
+    gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+        
+    ret, corners = cv2.findChessboardCorners(gray, CHECKERBOARD, cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
+
+    if ret == True:
+
+        objpoints.append(objp)
+
+        corners2 = cv2.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
+
+        imgpoints.append(corners2)
+
+        image = cv2.drawChessboardCorners(image, CHECKERBOARD, corners2, ret)
+
+    cv2.imshow('img', image)
+    cv2.waitKey(0)
+
+cv2.destroyAllWindows()
+
+__, mtx, dist, __, __ = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+fs =  cv2.FileStorage("camera_calibration.yaml", cv2.FILE_STORAGE_WRITE)
+fs.write("camera_matrix", mtx)
+fs.write("distortion_coeffs", dist)
+fs.release()
+    
--- dji_tello_camera_calibrate.py
+++ dji_tello_camera_calibrate.py
@@ -1,7 +1,7 @@
-# https://learnopencv.com/camera-calibration-using-opencv/
 import time, cv2
 from threading import Thread
 from djitellopy import Tello
+
 
 tello = Tello()
 tello.connect()
@@ -9,12 +9,11 @@ tello.connect()
 keepRecording = True
 tello.streamon()
 frame_read = tello.get_frame_read()
-image_list = []
 
 
 def video_stream():
     global keepRecording
-    global image_list
+    image_count = 0
     
     while keepRecording:
         
@@ -23,21 +22,22 @@ def video_stream():
         if frame is not None and frame.size > 0:
             
             cv2.imshow("Tello Live Stream", frame)
-            image_list.append(frame)
+            cv2.imwrite(f"calibration_img{image_count}.jpg", frame)
+            image_count += 1
             
         key = cv2.waitKey(1) & 0xFF
         
         if key == ord('q'):
             keepRecording = False
             
-        time.sleep(1)
+        time.sleep(5)
     cv2.destroyAllWindows()
 
 recorder = Thread(target=video_stream)
 recorder.start()
 
 try:
-    time.sleep(15)  
+    time.sleep(60)  
 finally:
     keepRecording = False
     recorder.join()
